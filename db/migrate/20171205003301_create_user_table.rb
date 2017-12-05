902c379801f950fca0239442f00b1b4807acc9f6
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-05 00:34:45 +0000

    automatically backed up by learn

--- db/migrate/20171205003301_create_user_table.rb
+++ db/migrate/20171205003301_create_user_table.rb
@@ -1,4 +1,9 @@
 class CreateUserTable < ActiveRecord::Migration
   def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password
+      t.real :balance
+    end
   end
 end
